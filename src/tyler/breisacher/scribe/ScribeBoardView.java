1183040b0847644561ac6085f0f9e152973eed17
Tyler Breisacher <matrixfrog@gmail.com>
2010-06-28 01:23:15 -0700

    Things are still going along quite well!

---
+++ src/tyler/breisacher/scribe/ScribeBoardView.java
@@ -0,0 +1,33 @@
+package tyler.breisacher.scribe;
+
+import tyler.breisacher.scribe.model.ScribeBoard;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+public class ScribeBoardView extends TableLayout {
+  private ScribeBoard scribeBoard;
+  
+  public ScribeBoardView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+  
+  public void setScribeBoard(ScribeBoard scribeBoard) {
+    this.scribeBoard = scribeBoard;
+    rebuildLayout();
+  }
+  
+  private void rebuildLayout() {
+    this.removeAllViews();
+    for (int y=0; y<3; y++) {
+      TableRow row = new TableRow(this.getContext());
+      this.addView(row);
+      for (int x=0; x<3; x++) {
+        MiniGridView miniGridView = new MiniGridView(this.getContext(), null);
+        miniGridView.setMiniGrid(this.scribeBoard.get(x, y));
+        row.addView(miniGridView);
+      }
+    }
+  }
+}
