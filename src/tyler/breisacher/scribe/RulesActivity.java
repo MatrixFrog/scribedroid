a3207ccf181343aae74ef96097bf62ef9e6ece47
Tyler Breisacher <matrixfrog@gmail.com>
2010-08-01 02:31:53 -0700

    Just a few minor things

--- src/tyler/breisacher/scribe/RulesActivity.java
+++ src/tyler/breisacher/scribe/RulesActivity.java
@@ -1,7 +1,9 @@
 package tyler.breisacher.scribe;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class RulesActivity extends Activity {
 
@@ -9,6 +11,13 @@ public class RulesActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.rules);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    startActivity(new Intent(this, GlyphActivity.class));
+    return false;
   }
 
 }
