8c1e19aa5f7815bf6c5765a34bbe4dc758fbbcee
Tyler Breisacher <matrixfrog@gmail.com>
2010-11-26 16:39:41 -0800

    Add a small delay to the AI so that you can kind of "see" where it's going.

--- src/tyler/breisacher/scribe/ai/RandomAIPlayer.java
+++ src/tyler/breisacher/scribe/ai/RandomAIPlayer.java
@@ -4,17 +4,40 @@ import tyler.breisacher.scribe.AIPlayer;
 import tyler.breisacher.scribe.Util;
 import tyler.breisacher.scribe.model.MiniGrid;
 import tyler.breisacher.scribe.model.XY;
+import android.os.AsyncTask;
 
 /**
  * A very stupid AI, simply to demonstrate the way an AI player could be written.
  */
 public class RandomAIPlayer extends AIPlayer {
+  private MiniGrid miniGrid;
+  private XY xy;
+
+  private final AsyncTask<Void, Void, Void> getTask() {
+    return new AsyncTask<Void, Void, Void>() {
+      @Override
+      protected Void doInBackground(Void... params) {
+        try {
+          Thread.sleep(750);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+        return null;
+      }
+
+      @Override
+      protected void onPostExecute(Void result) {
+        super.onPostExecute(result);
+        RandomAIPlayer.this.move(miniGrid, xy);
+      }
+    };
+  }
 
   @Override
   public void itsYourTurn() {
-    MiniGrid miniGrid = Util.choice(board.getEnabledMinigrids());
-    XY move = Util.choice(miniGrid.getEmptyCells());
-    move(miniGrid, move);
+    miniGrid = Util.choice(board.getEnabledMinigrids());
+    xy = Util.choice(miniGrid.getEmptyCells());
+    getTask().execute();
   }
 
 }
